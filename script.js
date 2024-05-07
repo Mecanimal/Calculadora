a5ccf09a7a3ab7359e297278eaf16be7b6ac54a3
Mecanimal <160550986+Mecanimal@users.noreply.github.com>
2024-05-07 18:53:07 +0000

    JS em andamento

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const display = document.getElementById('display');
+const one = document.getElementById('1');
+const c = document.getElementById('C');
 
+let currentValue = 0;
+let operation = 0;
+let operationValue = 0;
+
+function cPress() {
+    display.value = 0
+    currentValue = 0
+}
+
+function keyPress(id) {
+if(operation == 0) {
+    let newValue = parseFloat(currentValue.toString() + id.toString());
+    display.value = newValue;
+    currentValue = newValue;
+} else {
+    let newOperationValue = parasefloat(operationValue.toString() + id.toString())
+    display.value = newValue.toString + operationSimbol.toString + newOperationValue.toString
+}
+}
+
+function operationPress(id) {
+    operation = 1
+    let operationSimbol = id
+    display.value = parseFloat(newValue.toString + operationSimbol)
+}
+
+c.addEventListener('click', cPress);
