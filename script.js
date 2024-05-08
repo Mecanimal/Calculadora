d500d77842b72dcd1891c1d5d97dbc5af302bde7
Mecanimal <160550986+Mecanimal@users.noreply.github.com>
2024-05-08 18:32:39 +0000

    bruh

--- script.js
+++ script.js
@@ -2,30 +2,70 @@ const display = document.getElementById('display');
 const one = document.getElementById('1');
 const c = document.getElementById('C');
 
+let on = 0;
 let currentValue = 0;
 let operation = 0;
 let operationValue = 0;
+let operationSymbol = "";
+let newValue = 0;
 
-function cPress() {
-    display.value = 0
-    currentValue = 0
+// botão pra ligar
+function onOff() {
+if(on == 0){
+    on = 1
+    display.placeholder = '0'
+} else {on = 0}
 }
 
+// botão que reseta
+function cPress() {
+    display.value = 0;
+    currentValue = 0;
+    operation = 0;
+    operationValue = 0;
+}
+
+// função para escrever os números
 function keyPress(id) {
 if(operation == 0) {
-    let newValue = parseFloat(currentValue.toString() + id.toString());
+    newValue = parseFloat(currentValue.toString() + id.toString());
     display.value = newValue;
-    currentValue = newValue;
 } else {
-    let newOperationValue = parasefloat(operationValue.toString() + id.toString())
-    display.value = newValue.toString + operationSimbol.toString + newOperationValue.toString
+    newValue = parseFloat(currentValue.toString());
+    console.log(operationValue.toString() , id.toString())
+    let newOperationValue = parseFloat(operationValue.toString() + id.toString());
+    display.value = newValue.toString() + operationSymbol + newOperationValue.toString();
+    operationValue = newOperationValue;
 }
+currentValue = newValue;
 }
 
+// função para fazer operações
 function operationPress(id) {
-    operation = 1
-    let operationSimbol = id
-    display.value = parseFloat(newValue.toString + operationSimbol)
+    if(operation == '1' & operationValue != 0) {
+        equalsTo()
+    }
+    operation = 1;
+    operationSymbol = id;
+    display.value = currentValue.toString() + operationSymbol;
+    keyPress()
 }
 
-c.addEventListener('click', cPress);
+// função para fazer a conta
+function equalsTo() {
+    operation = 0;
+if(operationSymbol == '+') {
+    newValue = parseFloat(currentValue) + parseFloat(operationValue)
+} else { if(operationSymbol == '-') {
+    newValue = parseFloat(currentValue) - parseFloat(operationValue)
+} else { if(operationSymbol == '*') {
+    newValue = parseFloat(currentValue) * parseFloat(operationValue)
+} else {
+    newValue = parseFloat(currentValue) / parseFloat(operationValue)
+}
+}
+}
+    currentValue = newValue;
+    display.value = newValue;
+    operationValue = 0;
+}
