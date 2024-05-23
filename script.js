2d8a7854b20c21649ce2a8184e8f5bc2a7989e8f
Mecanimal <160550986+Mecanimal@users.noreply.github.com>
2024-05-23 18:54:41 +0000

    + - * / √ "working"

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const display = document.getElementById('display');
 
 let currentValue = ''
 
+// função para digitar algo
 function keyPress(id) {
     if(id === '=') {
         stringReader(currentValue)
@@ -12,20 +13,65 @@ function keyPress(id) {
     currentValue += value
 }
 
-// função para somar dois números naturais
+// função para resolver a expressão
 /**
  * 
  * @param {String} expression 
  */
 function stringReader(expression) {
-    expression = String(expression)
-    if(/(?=\d)+(?=\d)/.test(expression)) {
-        const [leftNumber, rightNumber] = expression.split('+')
-        console.log(parseFloat(leftNumber) + parseFloat(rightNumber))
+    if(/root\d/.test(expression)) {
+        if(!Number.isNaN(parseFloat(expression.split('root')[1]))) {
+            expression = expression.replace('root', '')
+            console.log(Math.sqrt(parseFloat(expression)))
+            return
+        }
     }
-    if(/\dminus\d/.test(expression)) {
+    if(String(expression).includes('root')) {
+        root(expression)
+        return
+    }
+    if(String(expression).startsWith('minus')) {
+        expression = expression.replace('minus', '-')
+    }
+    if(/\dminus|minusminus\d/.test(expression)) {
+        if(expression.includes('minusminus')) {
+            expression = expression.replace('minusminus', '+')
+            add(expression)
+            return
+        }
+        console.log(expression)
+        expression = expression.replace('minus', '-')
         const [leftNumber, rightNumber] = expression.split('minus')
-        console.log(leftNumber, rightNumber)
         console.log(parseFloat(leftNumber) - parseFloat(rightNumber))
+        return
     }
+    if(/\dover|overminus\d/.test(expression)) {
+        expression = expression.replace('minus', '-')
+        const [leftNumber, rightNumber] = expression.split('over')
+        console.log(parseFloat(leftNumber) / parseFloat(rightNumber))
+        return
+    }
+    if(/\dtimes|timesminus\d/.test(expression)) {
+        expression = expression.replace('minus', '-')
+        const [leftNumber, rightNumber] = expression.split('times')
+        console.log(parseFloat(leftNumber) * parseFloat(rightNumber))
+        return
+    }
+    expression = expression.replace('minus', '-')
+    add(expression)
+}
+
+// função para somar
+function add(expression) {
+    console.log(expression)
+    const [leftNumber, rightNumber] = expression.split('+')
+    console.log(parseFloat(leftNumber) + parseFloat(rightNumber))
+}
+
+function root(expression) {
+    console.log(expression)
+    expression = expression.replace('root', '')
+    const [leftNumber, rightNumber] = expression.split('+')
+    expression = Math.sqrt(parseFloat(leftNumber)).toString() + '+' + rightNumber.toString()
+    stringReader(expression)
 }
